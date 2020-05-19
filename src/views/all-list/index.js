a55c694da6061e9dd6e04de2a792f15091b997a5
menkan <xtz17515@163.com>
2020-05-19 13:56:07 +0800

    fix

--- src/views/all-list/index.js
+++ src/views/all-list/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-// import "./index.scss";
+import "./index.scss";
 
 export default class AllList extends React.Component {
   render() {
     return (
-      <div className="home-container" style={{ color: "#FFF" }}>
-        列表list...
+      <div className="all-listbox" style={{ color: "#FFF" }}>
+        <ul>
+          <li>20200202 <a href="/x">xxxxxxxxxxx</a></li>
+          <li>20200202 >> <a href="/1">xxxxxxxxxxx</a></li>
+          <li>20200202 >> <a href="/2">xxxxxxxxxxx</a></li>
+          <li>20200202 >> <a href="/3">xxxxxxxxxxx</a></li>
+          <li>20200202 >> <a href="/4">xxxxxxxxxxx</a></li>
+        </ul>
       </div>
     );
   }
