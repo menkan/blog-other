9a7b648d86eeb6982eeb2f2f8d08fe6e6cc2e104
menkan <xtz17515@163.com>
2020-05-21 22:36:54 +0800

    some styles

--- src/views/all-list/index.js
+++ src/views/all-list/index.js
@@ -5,12 +5,26 @@ export default class AllList extends React.Component {
   render() {
     return (
       <div className="all-listbox" style={{ color: "#FFF" }}>
+        <div className="allist-year-railing">
+          <span>2020</span>
+        </div>
         <ul>
-          <li>20200202 <a href="/x">xxxxxxxxxxx</a></li>
-          <li>20200202 >> <a href="/1">xxxxxxxxxxx</a></li>
-          <li>20200202 >> <a href="/2">xxxxxxxxxxx</a></li>
-          <li>20200202 >> <a href="/3">xxxxxxxxxxx</a></li>
-          <li>20200202 >> <a href="/4">xxxxxxxxxxx</a></li>
+          <li>
+            <span>20200202</span> &gt;&gt;
+            <a href="/x">ios a something bugs</a>
+          </li>
+          <li>
+            <span>20200202</span> &gt;&gt;
+            <a href="/x">xcode in version 9.2; crash logs</a>
+          </li>
+          <li>
+            <span>20200202</span> &gt;&gt;
+            <a href="/x">load and initialize event in oc</a>
+          </li>
+          <li>
+            <span>20200202</span> &gt;&gt;
+            <a href="/x">node + express chagnes</a>
+          </li>
         </ul>
       </div>
     );
